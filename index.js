fa8e9097e267814b34c226b28179b6bd7bdb57eb
Doharbdjr <119146391+Doharbdjr@users.noreply.github.com>
2024-09-03 19:28:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var keys = [
   "Uranus",
   "Neptune"
 ];
+
 var slider = new Swiper(".swiper-container", {
-  // Optional parameters
   slidesPerView: "auto",
   spaceBetween: 150,
   centeredSlides: true,
@@ -29,51 +29,34 @@ var slider = new Swiper(".swiper-container", {
 
 slider.on("slideChange", function() {
   console.log("SLIDE CHANGED");
-  gsap.to(".slide-text span", 0.2, {
-    x: "-100px"
+
+  gsap.to(".slide-text span, .slide-number span, .slide-detail span, .slide-detail-facts div", 0.2, {
+    x: "-100px",
+    opacity: 0,
+    stagger: 0.05
   });
-  gsap.to(".slide-number span", 0.2, {
-    x: "-100px"
-  });
-  gsap.to(".slide-detail span", 0.5, {
-    x: "-1000px"
-  });
-  gsap.to(".slide-detail-facts div", 0.5, {
-    x: "-1000px"
-  });
+
   gsap.to(".swiper-slide-active", 0.5, {
     scale: 0.85
   });
+
   gsap.to(".swiper-slide .slide-img", 1, {
     rotation: 20
   });
 });
 
 slider.on("slideChangeTransitionEnd", function() {
-  gsap.to(".swiper-slide .slide-img", 1, {
-    rotation: 10
+  gsap.to(".slide-text span, .slide-number span", 0.2, {
+    x: "100px",
+    opacity: 0,
+    onComplete: function() {
+      gsap.to(".slide-text span, .slide-number span", {x: 0, opacity: 1});
+    }
   });
-  gsap.to(".slide-text span", 0.2, {
+
+  gsap.to(".slide-detail span, .slide-detail-facts div", 0.5, {
     x: 0,
-    delay: 0.1
-  });
-  gsap.to(".slide-text span", 0, {
-    x: "100px"
-  });
-
-  gsap.to(".slide-number span", 0.2, {
-    x: 0
-  });
-  gsap.to(".slide-number span", 0, {
-    x: "100px"
-  });
-
-  gsap.to(".slide-detail span", 0.2, {
-    x: 0
-  });
-
-  gsap.to(".slide-detail-facts div", 0.5, {
-    x: 0
+    opacity: 1
   });
 
   gsap.to(".swiper-slide-active", 0.5, {
@@ -81,24 +64,15 @@ slider.on("slideChangeTransitionEnd", function() {
     ease: Power4.easeOut
   });
 
-  gsap.to(".swiper-slide-active .slide-text", 0, {
-    autoAlpha: 1
-  });
-  gsap.to(".swiper-slide-active .slide-number", 0, {
-    autoAlpha: 1
+  gsap.to(".swiper-slide .slide-img", 1, {
+    rotation: 10
   });
 
-  gsap.to(".swiper-slide-next .slide-text", 0, {
+  // Reset opacity for next/prev slide texts and numbers
+  gsap.set(".swiper-slide-next .slide-text, .swiper-slide-prev .slide-text", {
     autoAlpha: 0
   });
-  gsap.to(".swiper-slide-prev .slide-text", 0, {
-    autoAlpha: 0
-  });
-
-  gsap.to(".swiper-slide-next .slide-number", 0, {
-    autoAlpha: 0
-  });
-  gsap.to(".swiper-slide-prev .slide-number", 0, {
+  gsap.set(".swiper-slide-next .slide-number, .swiper-slide-prev .slide-number", {
     autoAlpha: 0
   });
 });
@@ -110,24 +84,12 @@ gsap.to(".rockbg1", 2, {
   delay: 0
 });
 
-gsap.to(".swiper-slide-next .slide-text", 0, {
+// Initial setup to ensure proper scaling and visibility
+gsap.set(".swiper-slide", {scale: 0.85});
+gsap.set(".swiper-slide-active", {scale: 1});
+gsap.set(".swiper-slide-next .slide-text, .swiper-slide-prev .slide-text", {
   autoAlpha: 0
 });
-gsap.to(".swiper-slide-prev .slide-text", 0, {
+gsap.set(".swiper-slide-next .slide-number, .swiper-slide-prev .slide-number", {
   autoAlpha: 0
-});
-
-gsap.to(".swiper-slide-next .slide-number", 0, {
-  autoAlpha: 0
-});
-gsap.to(".swiper-slide-prev .slide-number", 0, {
-  autoAlpha: 0
-});
-
-gsap.to(".swiper-slide", 0, {
-  scale: 0.85
-});
-
-gsap.to(".swiper-slide-active", 0, {
-  scale: 1
 });
